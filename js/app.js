14d5663a60e56b8714ac6e32b86292c4ff54686c
Maythe <maytheinfante96@gmail.com>
2018-01-08 11:05:38 -0500

    enlazando botones signup y login

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
 // cargando todo el árbol de nodos del DOM
 $(document).ready(function() {
-  var $buttonNewUser = $('#newUser');
-  console.log($buttonNewUser);
-  var $buttonLogIn = $('#logIn');
-});
-// agregando evento click para los botones
-$buttonNewUser.on('click', function() {
-  window.location.href = 'signup.html';
-});
-$buttonLogI.on('click', function() {
-  window.location.href = 'login.html';
+
 });
