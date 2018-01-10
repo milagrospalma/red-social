b1ad0a78ca81f355be809c31e736523ec2eef159
MYTH-INF <maytheinfante96@gmail.com>
2018-01-09 22:42:34 -0500

    agregado cambios de funcionalidad

--- js/signup.js
+++ js/signup.js
@@ -6,16 +6,17 @@ $(document).ready(function() {
   var $btnRegistro = $('.button-registro');
   // CREANDO FUNCION QUE VALIDA LOS DATOS DEL IMPUT PASSWORD
   $password.on('input', function() {
-    // CREANDO VARIABLE QUE VERIFICA NUMERO DE TELEFONO
     if ($password .val().length >= 6) {
       $btnFrase.attr('disabled', false);
     } else {
       $btnFrase.attr('disabled', true);
     }
   });
+  // CREANDO EVENTO CLICK QUE AGREGA IMAGEN DE FRASES DE MANERA ALEATORIA
   $btnFrase.on('click', function() {
     var min = 1;
     var max = 5;
+    //  CREANDO VARIALE PARA IMAGENES DE FRASES ALEATORIAS
     var aleatorio = Math.floor(Math.random() * (max - (min - 1))) + min;
     var imagePhrase = '<img src="../assets/images/frase' + aleatorio + '.png' + '">';
     var phraseContainer = $('.frase'); // div que contiene el botón de frase y almacenará el img-frase
