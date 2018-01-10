cea13c6f712969da59727834f64d765a458eab69
aurora <selva_beat@hotmail.com>
2018-01-10 18:20:25 -0500

    finalizando funcion

--- js/home.js
+++ js/home.js
@@ -2,10 +2,6 @@ $(document).ready(function() {
   // obteniendo datos para agregar al header 
   var $nickName = localStorage.getItem('nick-name');
   $('div.div-img>p').append($nickName);
-  // var $frase = localStorage.getItem('img-frase');
-  // $frase.attr('class', 'frase');
-  // $('.div-frase').append($frase);
-
   // funcionalidad para el modal 'comentar'
   var $iconComment = $('.icon-comment');
   // evento para el icono 'comment'
@@ -24,9 +20,12 @@ $(document).ready(function() {
     // funcionalidad para el button y icon del modal
     $('.button-modal').on('click', function() {
       var $inputValue = $('div.add-comment>input').val();
-      $('.comments').append('<span class="color-span">' + $nickName + ':' + ' </span>' + '<p class="color-p">' + $inputValue + '</p>' + '<br>');
+      $('.comments').append('<span class="color-span">' + $nickName + ':' + ' </span>' + '<p class="color-p">' + $inputValue + '<br>' + moment().format('LLLL') + '</p>' + '<br>');
+      $inputValue = $('div.add-comment>input').val('');
+
     });
     $('.icon-close').on('click', function() {
+      $('.comments').empty();
       $('.section-modal').attr('hidden', 'hidden');
     });
   });
@@ -46,7 +45,7 @@ $(document).ready(function() {
       var $inputText = $('.add-text');
       var $inputLink = $('.add-link');
       var $inputTitle = $('.add-title');
-      var $inputVideo = $('.add-video')
+      var $inputVideo = $('.add-video');
       // evento para los inputs del modal
       $inputText.on('keyup', function(event) {
         var $inputTextValue = $inputText.val();
@@ -72,34 +71,73 @@ $(document).ready(function() {
       });
       // evento del button del modal: agregando elementos al html
       $('.button-modal').on('click', function() {
-        var $new = $('.new');
-        $('.row-news').append($new);
+        $('p>a.nick-name').empty();
         $('p>a.nick-name').append($nickName);
+        // evento para el input 'add-text'
         var $inputTextValue = $inputText.val();
         $('div.card-content>p.text').empty();
         $('div.card-content>p.text').append($inputTextValue);
+        $inputTextValue = $inputText.val('');
+        // evento para el input 'add-link'
         var $inputLinkValue = $inputLink.val();
         $('div.card-content>a.link').removeAttr('href');
         $('div.card-content>a.link').attr('href', $inputLinkValue);
+        $inputLinkValue = $inputLink.val('');
+        // evento para el input 'add-title'
         var $inputTitleValue = $inputTitle.val();
         $('div.card-content>span.title').empty();
         $('div.card-content>span.title').append($inputTitleValue);
+        $inputTitleValue = $inputTitle.val('');
+        // evento para el input 'add-video'
         var $inputVideoValue = $inputVideo.val();
-        $('div.video>iframe').removeAttr('src');
-        $('div.video>iframe').attr('src', $inputVideoValue);
+        $('div.card-content>iframe.video').removeAttr('src');
+        $('div.card-conten>iframe.video').attr('src', $inputVideoValue);
+        $inputVideoValue = $inputVideo.val('');
+        // creando nuevos contenidos
+        var $new = $('.new');
+        $($new).clone().appendTo('.row-news');
+        // deshabilitando modal2
         $('.section-modal2').attr('hidden', 'hidden');
       });
     });
+    // evento para los iconos 'input-file'
     $('.icon-input-file').on('mouseover', function() {
       $('.input-field').removeAttr('hidden');
       $('.input-text').attr('hidden', 'hidden');
-      var $inputFile = $('div.input-field>input');
-      $inputFile.on('click', function(event) {
-        $('.button-modal').removeAttr('disabled');
-        // $('.button-modal').attr('disabled', 'disabled');
+      var $inputTitle = $('.add-title');
+      var $inputImage = $('add-img');
+      // evento para los inputs del modal
+      $inputTitle.on('keyup', function(event) {
+        var $inputTitleValue = $inputTitle.val();
+        $('.button-modal').removeAttr('disabled');  
+        if ($inputTitleValue === '' || event.keyCode === 69 && $inputTitleValue.lenght <= 1) {
+          $('.button-modal').attr('disabled', 'disabled');
+        }
+      });
+      $inputImage.on('click', function() {
+        var $inputImageValue = $inputImage.val();
+        $('.button-modal').removeAttr('disabled');  
+      });
+      // evento del button del modal: agregando elementos al html
+      $('.button-modal').on('click', function() {
+        $('p>a.nick-name').empty();
+        $('p>a.nick-name').append($nickName);
+        // evento para el input 'add-title'
+        var $inputTitleValue = $inputTitle.val();
+        $('div.card-content>span.title').empty();
+        $('div.card-content>span.title').append($inputTitleValue);
+        $inputTitleValue = $inputTitle.val('');
+        // evento para el input 'add-img'
+        var $inputImageValue = $inputImage.val();                
+        $('div.card-content>img.img').removeAttr('src');
+        $('div.card-content>img.img').attr('src', '../assets/images/' + $inputImageValue);
+        $inputImageValue = $inputImage.val('');
+        // creando nuevos contenidos
+        var $new = $('.new');
+        $($new).clone().appendTo('.row-news');
+        // deshabilitando modal2
+        $('.section-modal2').attr('hidden', 'hidden');
       });
     });
-
-    $('.button-modal').on('click', function() {});
   });
-});
+ });
--- js/signup.js
+++ js/signup.js
@@ -25,9 +25,6 @@ $(document).ready(function() {
     $btnRegistro.attr('disabled', false);
   });
   // CREANDO FUNCION QUE GUARDA DATOS EN EL NAVEGADOR
-  // $btnFrase.on('click', function() {
-  //   localStorage.setItem('img-frase', $imagePhrase.val());
-  // });
   $btnRegistro.on('click', function() {
     localStorage.setItem('nick-name', $nickName.val());
   });
